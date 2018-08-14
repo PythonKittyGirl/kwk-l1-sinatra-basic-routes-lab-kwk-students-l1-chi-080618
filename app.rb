470428671c09228b80eddb933c90d1dd1ddd64ca
PythonKittyGirl <ung.deborah@gmail.com>
2018-08-14 19:05:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+  "My name is Isabel"
+  
+  get '/hometown' do
+  "Warrenville"
+  
+  get '/favorite-song' do
+  "My favorite song is Baba Yetu"
+end
+end
+end
 end
